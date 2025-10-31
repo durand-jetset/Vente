e5309db4c161edbbf34e49c16c030f1f3cd670f9
durand-jetset <109703050+durand-jetset@users.noreply.github.com>
2025-10-31 05:11:35 +0000

    Add files via upload

--- app.py
+++
@@ -1,7 +0,0 @@
-echo "# Vente" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/durand-jetset/Vente.git
-git push -u origin main
